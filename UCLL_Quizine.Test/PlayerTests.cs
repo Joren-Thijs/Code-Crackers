8a5ec834fd90ba6798219bcd8dc70309f7b5c598
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-03-30 00:07:48 +0200

    Added tests for multiple players

--- UCLL_Quizine.Test/PlayerTests.cs
+++ UCLL_Quizine.Test/PlayerTests.cs
@@ -14,5 +14,47 @@ namespace UCLL_Quizine.Test
             var result = game.AddPlayer(gameCode, "John");
             Assert.IsTrue(result);
         }
+
+        [Test]
+        public void AddTwoPlayersTest()
+        {
+            var gameCode = "abcd";
+            var maxNumberOfPlayers = 4;
+            var game = new Game(gameCode, maxNumberOfPlayers);
+
+            var result = 
+                game.AddPlayer(gameCode, "John") &&
+                game.AddPlayer(gameCode, "Harold");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void AddThreePlayersTest()
+        {
+            var gameCode = "abcd";
+            var maxNumberOfPlayers = 4;
+            var game = new Game(gameCode, maxNumberOfPlayers);
+
+            var result =
+                game.AddPlayer(gameCode, "John") &&
+                game.AddPlayer(gameCode, "Harold") &&
+                game.AddPlayer(gameCode, "Sarah");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void AddFourPlayersTest()
+        {
+            var gameCode = "abcd";
+            var maxNumberOfPlayers = 4;
+            var game = new Game(gameCode, maxNumberOfPlayers);
+
+            var result =
+                game.AddPlayer(gameCode, "John") &&
+                game.AddPlayer(gameCode, "Harold") &&
+                game.AddPlayer(gameCode, "Sarah") &&
+                game.AddPlayer(gameCode, "Linda");
+            Assert.IsTrue(result);
+        }
     }
 }
