85fd632b87d1f1377e236e13a2262a2aacf3f939
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-03-30 18:23:51 +0200

    added question constructors and tests.

---
+++ UCLL_Quizine.Test/QuestionTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UCLL_Quizine.Test
+{
+    public class QuestionTests
+    {
+        [Test]
+        public void CreateQuestionTest()
+        {
+            var question = new Question();
+            Assert.IsNotNull(question);
+        }
+
+        [Test]
+        public void QuestionTextIsNotNullTest()
+        {
+            var question = new Question();
+            Assert.IsNotNull(question.QuestionText);
+        }
+
+        [Test]
+        public void QuestionAnswersIsNotNullTest()
+        {
+            var question = new Question();
+            Assert.IsNotNull(question.Answers);
+        }
+
+        [Test]
+        public void QuestionCorrectAnswerIdsIsNotNullTest()
+        {
+            var question = new Question();
+            Assert.IsNotNull(question.CorrectAnswerIds);
+        }
+    }
+}
--- UCLL_Quizine/Question.cs
+++ UCLL_Quizine/Question.cs
@@ -4,8 +4,17 @@ namespace UCLL_Quizine
 {
     public class Question
     {
-        public string QuestionText { get; set; }
-        public List<Answer> Answers { get; set; }
-        public List<char> CorrectAnswerIds { get; set; }
+        public Question() {}
+
+        public Question(string questionText, List<Answer> answers, List<char> correctAnswerIds)
+        {
+            QuestionText = questionText;
+            Answers = answers;
+            CorrectAnswerIds = correctAnswerIds;
+        }
+
+        public string QuestionText { get; set; } = "";
+        public List<Answer> Answers { get; set; } = new List<Answer>();
+        public List<char> CorrectAnswerIds { get; set; } = new List<char>();
     }
 }
