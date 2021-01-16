3ca66f7672f54ed0c077e2dfaa4da08542303f7a
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2021-01-16 20:25:01 +0100

    Added QuizHub

--- CodeCrackers.API/Startup.cs
+++ CodeCrackers.API/Startup.cs
@@ -1,3 +1,4 @@
+using CodeCrackers.API.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,7 @@ namespace CodeCrackers.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CodeCrackers.API", Version = "v1" });
             });
+            services.AddSignalR
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +55,7 @@ namespace CodeCrackers.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<QuizHub>("/quizhub");
             });
         }
     }
